b102e8891a8fa280c6a79f0b67ea5c2bd213ae9b
tonyx <tonyx@yahoo.com>
2010-03-02 00:33:22 +0100

    factory refactoring

--- ocpBowling/KataGameFactory.cs
+++ ocpBowling/KataGameFactory.cs
@@ -8,35 +8,62 @@ namespace ocpBowling
     {
         public static Bowling getTerrestrialBowling()
         {
-            Bowling genericGenericBowling = new FlexibleBowling();
-            List<RuleForFrame> ruleForPlainFrame =   new List<RuleForFrame>(){new TerrestrianStrikeRule(),new TerrestrianSpareRule()};
-            
-            List<RuleForFrame> rulesForLastFrame = new List<RuleForFrame>();
-            rulesForLastFrame.Add(new TerrestrianLastFrameRule());
-            for (int i =0;i<9;i++)
-                genericGenericBowling.AddConstraint(x => x.Rolls.Sum()<=10&&((x.Rolls[0]==10&&x.Rolls.Count==1)||(x.Rolls[0]<10&&x.Rolls.Count>1)));            
-            genericGenericBowling.AddConstraint(x => x.Rolls.Sum()<=30 && (x.Rolls[0]<10||x.Rolls.Count>1));       
-            for (int i = 0; i < 9;i++ )
-                genericGenericBowling.AddRulesForFrame(ruleForPlainFrame);
-            genericGenericBowling.AddRulesForFrame(rulesForLastFrame);                                   
-            genericGenericBowling.Init();
-
-            return genericGenericBowling;
+            Bowling terrestrialBowling = new FlexibleBowling();
+            SetConstraintsForFirstNineFrames(terrestrialBowling);
+            SetTerrestrianBonusRules(terrestrialBowling);
+            terrestrialBowling.Init();
+            return terrestrialBowling;
         }
 
         public static Bowling getMartianBowling()
         {
             Bowling martianBowling = new FlexibleBowling();
-            martianBowling.AddConstraint(x => ((x.Rolls.Count == 3 &&  x.Rolls.Sum()<=10)||(x.Rolls.Count<3 && x.Rolls.Sum() == 10)));
-            martianBowling.AddConstraint(x => ((x.Rolls.Count == 3 &&  x.Rolls.Sum()<=10)||(x.Rolls.Count<3 && x.Rolls.Sum() == 10)));
-            martianBowling.AddConstraint(x => ((x.Rolls.Count == 3 &&  x.Rolls.Sum()<=10)||(x.Rolls.Count<3 && x.Rolls.Sum() == 10)));
-            martianBowling.AddRulesForFrame(new List<RuleForFrame>{new MartianFrameBonus()});
-            martianBowling.AddRulesForFrame(new List<RuleForFrame>{new MartianFrameBonus()});
-            martianBowling.AddRulesForFrame(new List<RuleForFrame>{new MartianFrameNoBonus()});
+
+            Constraint upToThreeRollsUnlessDropTenEarlier =
+                x => ((x.Rolls.Count == 3 && x.Rolls.Sum() <= 10) || (x.Rolls.Count < 3 && x.Rolls.Sum() == 10));
+            
+            martianBowling.AddConstraint(upToThreeRollsUnlessDropTenEarlier);
+            martianBowling.AddConstraint(upToThreeRollsUnlessDropTenEarlier);
+            martianBowling.AddConstraint(upToThreeRollsUnlessDropTenEarlier);
+
+            martianBowling.AddRulesForFrame(new List<RuleForFrame> { new MartianFrameBonus() });
+            martianBowling.AddRulesForFrame(new List<RuleForFrame> { new MartianFrameBonus() });
+            martianBowling.AddRulesForFrame(new List<RuleForFrame> { new MartianFrameNoBonus() });
             martianBowling.Init();
             return martianBowling;
         }
 
 
+
+        private static void SetTerrestrianBonusRules(Bowling terrestrialBowling)
+        {
+            List<RuleForFrame> ruleForPlainFrame = new List<RuleForFrame> { new TerrestrianStrikeRule(), new TerrestrianSpareRule() };
+            List<RuleForFrame> rulesForLastFrame = new List<RuleForFrame> { new TerrestrianLastFrameRule() };
+
+            for (int i = 0; i < 9;i++ )
+                terrestrialBowling.AddRulesForFrame(ruleForPlainFrame);
+            
+            terrestrialBowling.AddRulesForFrame(rulesForLastFrame);
+        }
+
+
+        private static void SetConstraintsForFirstNineFrames(Bowling terrestrialBowling)
+        {
+            Constraint sumOfAllRollMustbeLessThanTen = (x => x.Rolls.Sum() <= 10);
+            Constraint frameWithStrikeHasOnlyOneRoll = (x => (x.Rolls[0] == 10 && x.Rolls.Count == 1));
+            Constraint frameWithNoStrikeHasTwoRolls = (x => (x.Rolls[0] < 10 && x.Rolls.Count == 2));
+            Constraint plainFrameConstraint =
+                (x =>
+                 (sumOfAllRollMustbeLessThanTen(x) &&
+                  (frameWithStrikeHasOnlyOneRoll(x) || frameWithNoStrikeHasTwoRolls(x))));
+
+            for (int i = 0; i < 9; i++)
+                terrestrialBowling.AddConstraint(plainFrameConstraint);
+
+            Constraint sumRollsNoHigherThanThirty = x => x.Rolls.Sum() <= 30;
+            Constraint ifFirstRollIsTenThanThereIsAtLeastAnotherRoll = x => x.Rolls[0]<10||x.Rolls.Count > 1;
+            
+            terrestrialBowling.AddConstraint(x => sumRollsNoHigherThanThirty(x) && ifFirstRollIsTenThanThereIsAtLeastAnotherRoll(x));
+        }
     }
 }
