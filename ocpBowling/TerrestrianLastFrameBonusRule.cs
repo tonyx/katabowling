fc6763df68ab2063593b0f766c94e214fc10e242
tonyx <tonyx@yahoo.com>
2010-03-08 19:15:22 +0100

    new files

---
+++ ocpBowling/TerrestrianLastFrameBonusRule.cs
@@ -0,0 +1,15 @@
+﻿namespace ocpBowling
+{
+    public class TerrestrianLastFrameRule : RuleForFrame
+    {
+        public int Bonus(Frame[] frames, int i)
+        {
+            return 0;
+        }
+
+        public bool ConditionToBreak(Frame[] frames, int i)
+        {
+            return true;
+        }
+    }
+}
---
+++ ocpBowling/TerrestrianSpareBonusRule.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+namespace ocpBowling
+{
+    public class TerrestrianSpareRule : RuleForFrame
+    {
+
+        public int Bonus(Frame[] frames, int i)
+        {
+            if (Matches(frames, i))
+                return frames[i + 1].Rolls[0];            
+            return 0;
+        }
+
+        private bool Matches(Frame[] frames, int i)
+        {
+            if (spare(frames[i]))
+                return true;
+            return false;
+        }
+
+        public bool ConditionToBreak(Frame[] frames, int i)
+        {
+            if (Matches(frames, i))
+                return true;
+            return false;
+        }
+
+        private bool spare(Frame frame)
+        {
+            return ((frame.Rolls.Sum(x => x))==10);
+        }
+
+    }
+}
